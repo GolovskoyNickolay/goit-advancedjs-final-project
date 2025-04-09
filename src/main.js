c3364c2f55781493dc183e22a91c445a7abf7a12
Andrii Pichkorskiy <93935023+AndriiPichkorkiy@users.noreply.github.com>
2025-04-09 20:37:58 +0300

    Save progress

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import './css/styles.css';
 
-
 // NOTE: just sample how to use api methods/show errors
 import {
   getExercises,
@@ -8,23 +7,23 @@ import {
   updateExerciseRating,
   getFilters,
   getQuoteOfTheDay,
-  subscribe
+  subscribe,
 } from './services/apiServices.js';
 
-async function init() {
-  const data = await getExercises({ bodypart: 'back' });
-  const data2 = await getExerciseById('123123123213');
-  await updateExerciseRating(123, { rating: 5 });
-  const filtersData = await getFilters({
-    filter: 'body parts',
-    page: 2,
-    limit: 10,
-  });
+// async function init() {
+//   const data = await getExercises({ bodypart: 'back' });
+//   const data2 = await getExerciseById('123123123213');
+//   await updateExerciseRating(123, { rating: 5 });
+//   const filtersData = await getFilters({
+//     filter: 'body parts',
+//     page: 2,
+//     limit: 10,
+//   });
 
-  const quoteData = await getQuoteOfTheDay();
-  const subscribeResponse = await subscribe('test@example.com');
+//   const quoteData = await getQuoteOfTheDay();
+//   const subscribeResponse = await subscribe('test@example.com');
 
-  console.log(data, data2, subscribeResponse, quoteData, filtersData)
-}
+//   console.log(data, data2, subscribeResponse, quoteData, filtersData)
+// }
 
-init();
+// init();
